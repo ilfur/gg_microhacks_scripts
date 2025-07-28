5f841787ffdadbada48a04a09000c8a855234c87
ilfur <35766086+ilfur@users.noreply.github.com>
2025-07-28 17:32:19 +0200

    Update config_goldengate.sh

--- config_goldengate.sh
+++ config_goldengate.sh
@@ -135,7 +135,7 @@ curl -X POST \
    ],
     "description": "Source HR Schema extract",
     "source": "tranlogs",
-    "type": "Integrated",
+    "type": "integrated",
     "registration": {
       "optimized": true
      },
@@ -182,7 +182,7 @@ curl -X POST \
    ],
     "description": "Target HR Schema extract",
     "source": "tranlogs",
-    "type": "Integrated",
+    "type": "integrated",
     "registration": {
       "optimized": true
      },
