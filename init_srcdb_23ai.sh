9eaeb4532ca1165cf37c4e5a60de0270f19da105
ilfur <35766086+ilfur@users.noreply.github.com>
2025-07-29 15:22:39 +0200

    Update init_srcdb_23ai.sh

--- init_srcdb_23ai.sh
+++ init_srcdb_23ai.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+#/bin/bash
+## Variables used:
+## SRC_URL    - source database URL, like ggadmin@db23ai.oracle23ai:1521/FREEPDB1
+## SRC_PWD    - source database password for URL, like Welcome1234#
+## SRC_USER   - source database user for goldengate
+## SRC_SCHEMA - schema to be synced in source database, like SH
+## ADMIN_PWD  - password for SYSTEM user of source PDB
+
+export SRC_URL="db23ai.oracle23ai:1521/FREEPDB1"
+export SRC_USER="ggadmin"
+export SRC_PWD="BrunhildeZ32##"
+export ADMIN_PWD="BrunhildeZ32##"
+
 # Loading the SH schema initially 
 curl -v https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/peq5JVouDUrhcYFEzQ8GCznr4PBcuQzgCG1a9NBnLeip6Z9qiD6x77bdfnO0e0er/n/frul1g8cgfam/b/hr-sh-sample-data/o/sh.dmp -o sh.dmp
-impdp SYSTEM/$ADMIN_PWD@//$DBHOST.$DBNAMESPACE:1521/freepdb1 directory=DATA_PUMP_DIR schemas=SH dumpfile=sh.dmp
+impdp SYSTEM/$ADMIN_PWD@//$SRC_URL directory=DATA_PUMP_DIR schemas=SH dumpfile=sh.dmp
 
 # Now creating GGADMIN user in PDB and granting him GoldenGate read and apply roles
-sqlplus SYSTEM/$ADMIN_PWD@//$DBHOST.$DBNAMESPACE:1521/freepdb1 <<EOF
+sqlplus SYSTEM/$ADMIN_PWD@//$SRC_URL <<EOF
 ALTER PLUGGABLE DATABASE ADD SUPPLEMENTAL LOG DATA ;
 ALTER DATABASE FORCE LOGGING;
 ALTER SYSTEM SWITCH LOGFILE;
-CREATE USER GGADMIN IDENTIFIED BY $ADMIN_PWD
+CREATE USER $SRC_USER IDENTIFIED BY $SRC_PWD
   DEFAULT TABLESPACE USERS
   TEMPORARY TABLESPACE TEMP;
-GRANT CONNECT, RESOURCE, OGG_CAPTURE, OGG_APPLY to GGADMIN;
+GRANT CONNECT, RESOURCE, OGG_CAPTURE, OGG_APPLY to $SRC_USER;
 EOF
 
 
