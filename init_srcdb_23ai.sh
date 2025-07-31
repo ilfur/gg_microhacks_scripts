bcad904eca23a85d361775ebd6a4ec221e4e1385
ilfur <35766086+ilfur@users.noreply.github.com>
2025-07-31 15:39:38 +0200

    Update init_srcdb_23ai.sh

--- init_srcdb_23ai.sh
+++ init_srcdb_23ai.sh
@@ -6,11 +6,6 @@
 ## SRC_SCHEMA - schema to be synced in source database, like SH
 ## ADMIN_PWD  - password for SYSTEM user of source PDB
 
-export SRC_URL="db23ai.oracle23ai:1521/FREEPDB1"
-export SRC_USER="ggadmin"
-export SRC_PWD="BrunhildeZ32##"
-export ADMIN_PWD="BrunhildeZ32##"
-
 # Loading the SH schema initially 
 # curl -v https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/peq5JVouDUrhcYFEzQ8GCznr4PBcuQzgCG1a9NBnLeip6Z9qiD6x77bdfnO0e0er/n/frul1g8cgfam/b/hr-sh-sample-data/o/sh.dmp -o sh.dmp
 # impdp SYSTEM/$ADMIN_PWD@//$SRC_URL directory=DATA_PUMP_DIR schemas=SH dumpfile=sh.dmp
