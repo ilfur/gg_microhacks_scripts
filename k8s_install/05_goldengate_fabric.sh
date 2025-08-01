96631a3114a28eee3e4da02930f17d56a733c951
ilfur <schello@ilfur.de>
2025-08-01 15:13:06 +0000

    added gg for distributed systems

---
+++ k8s_install/05_goldengate_fabric.sh
@@ -0,0 +1,12 @@
+helm show values oggfree/goldengate-bigdata >ggfabric.yaml
+#putting the external address into the goldengate deployment
+sed -i "s/ggate.141.147.33.9/ggfabric.${EXTIP// /}/g" ggfabric.yaml
+
+#sample container pull secret, only required for GG bigdata
+#  kubectl create secret docker-registry container-registry-secret -n microhacks  --docker-username=marcel.pfeifer@oracle.com --docker-password=yadayada --docker-server=container-registry.oracle.com
+
+
+#BEFORE running the install, please exchange the target database connection string in gghack.yaml !!!
+
+helm install oggfabric oggfree/goldengatebigdata --values ggfabric.yaml -n microhacks
+
