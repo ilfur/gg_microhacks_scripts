43f35fb2e08961c3211399d093e1a866e4823608
ilfur <35766086+ilfur@users.noreply.github.com>
2025-08-06 16:31:30 +0200

    Update 03_ingress_nginx.sh

--- k8s_install/03_ingress_nginx.sh
+++ k8s_install/03_ingress_nginx.sh
@@ -1,8 +1,7 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 kubectl create namespace ingress-nginx
-helm install nginx-quick ingress-nginx/ingress-nginx -n ingress-nginx --values nginx-values.yaml
-
+helm install nginx-quick ingress-nginx/ingress-nginx -n ingress-nginx 
 #get external IP of nginx controller
 kubectl get service -n ingress-nginx -o jsonpath='{range .items[*]} {.status.loadBalancer.ingress[*].ip} {"\n"} {end}'
 
