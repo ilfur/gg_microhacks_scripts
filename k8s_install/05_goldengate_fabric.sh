1a5769010e8a47eb9bf5efce572e8936f6b8ca0c
ilfur <35766086+ilfur@users.noreply.github.com>
2025-08-25 14:01:36 +0200

    Update 05_goldengate_fabric.sh

--- k8s_install/05_goldengate_fabric.sh
+++ k8s_install/05_goldengate_fabric.sh
@@ -1,4 +1,6 @@
 helm show values oggfree/goldengate-bigdata >ggfabric.yaml
+#getting public address of nginx ingress controller
+export EXTIP=$(kubectl get service -n ingress-nginx -o jsonpath='{range .items[*]} {.status.loadBalancer.ingress[*].ip} {end}')
 #putting the external address into the goldengate deployment
 sed -i "s/ggate.141.147.33.9/ggfabric.${EXTIP// /}/g" ggfabric.yaml
 
