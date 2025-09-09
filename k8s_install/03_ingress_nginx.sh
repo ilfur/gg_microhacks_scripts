128741df2945dd28f573d2131fefb0677068126d
ilfur <35766086+ilfur@users.noreply.github.com>
2025-09-09 14:41:14 +0200

    Update 03_ingress_nginx.sh

--- k8s_install/03_ingress_nginx.sh
+++ k8s_install/03_ingress_nginx.sh
@@ -4,4 +4,4 @@ kubectl create namespace ingress-nginx
 helm install nginx-quick ingress-nginx/ingress-nginx -n ingress-nginx 
 #get external IP of nginx controller
 kubectl get service -n ingress-nginx -o jsonpath='{range .items[*]} {.status.loadBalancer.ingress[*].ip} {"\n"} {end}'
-
+kubectl patch service nginx-quick-ingress-nginx-controller -n ingress-nginx -p '{\"metadata\":{\"annotations\":{\"service.beta.kubernetes.io/azure-load-balancer-health-probe-request-path\":\"/healthz\"}}}'
