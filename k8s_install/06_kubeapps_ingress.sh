79e3b3e6d3588e8945afae19cf40599373926547
ilfur <35766086+ilfur@users.noreply.github.com>
2025-09-09 14:42:15 +0200

    Delete k8s_install/06_kubeapps_ingress.sh

--- k8s_install/06_kubeapps_ingress.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#getting public address of nginx ingress controller
-export EXTIP=$(kubectl get service -n ingress-nginx -o jsonpath='{range .items[*]} {.status.loadBalancer.ingress[*].ip} {end}')
-
-sed -i "s/kubeapps.84-235-173-41.nip.io/kubeapps.${EXTIP// /}/g" 06_kubeapps_ingress.yaml
-
-kubectl apply -f 06_kubeapps_ingress.yaml
