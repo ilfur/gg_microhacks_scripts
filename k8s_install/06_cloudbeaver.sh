98991c2b62484524dc3486cb903dda0c92bd6b20
ilfur <35766086+ilfur@users.noreply.github.com>
2025-09-09 14:44:33 +0200

    Update 06_cloudbeaver.sh

--- k8s_install/06_cloudbeaver.sh
+++ k8s_install/06_cloudbeaver.sh
@@ -1,3 +1,24 @@
 helm repo add avisto https://avistotelecom.github.io/charts/
 kubectl create namespace cloudbeaver
 helm install cloudbeaver avisto/cloudbeaver --version 1.0.1 -n cloudbeaver
+
+kubectl apply -f - <<EOF
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: cloudbeaver-ingress
+  namespace: cloudbeaver
+spec:
+  ingressClassName: nginx
+  rules:
+  - host: beaver.84-235-173-41.nip.io
+    http:
+      paths:
+      - backend:
+          service:
+            name: cloudbeaver
+            port:
+              number: 8978
+        path: /
+        pathType: Prefix
+EOF
