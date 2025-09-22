c96fd4ae2cbf82901e9627cfa079e1c5f3d225ed
ilfur <35766086+ilfur@users.noreply.github.com>
2025-09-22 11:26:43 +0200

    Update 07_estate_explorer.sh

--- k8s_install/07_estate_explorer.sh
+++ k8s_install/07_estate_explorer.sh
@@ -2,7 +2,7 @@ helm show values oggfree/autonomous-free >autonomous.yaml
 #getting public address of nginx ingress controller
 export EXTIP=$(kubectl get service -n ingress-nginx -o jsonpath='{range .items[*]} {.status.loadBalancer.ingress[*].ip} {end}')
 #putting the external address into the goldengate deployment
-sed -i "s/131.189.32.182/${EXTIP// /}/g" autonomous.yaml
+sed -i "s/xxx-xxx-xxx-xxx/${EXTIP// /}/g" autonomous.yaml
 
 
 #BEFORE running the install, please exchange the database password in autonomous.yaml !
