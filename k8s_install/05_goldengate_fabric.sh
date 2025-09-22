bb1f5e2d5c1fbad6e94864ea0334b8bb2f15bbfa
ilfur <35766086+ilfur@users.noreply.github.com>
2025-09-22 11:25:59 +0200

    Update 05_goldengate_fabric.sh

--- k8s_install/05_goldengate_fabric.sh
+++ k8s_install/05_goldengate_fabric.sh
@@ -2,7 +2,7 @@ helm show values oggfree/goldengate-bigdata >ggfabric.yaml
 #getting public address of nginx ingress controller
 export EXTIP=$(kubectl get service -n ingress-nginx -o jsonpath='{range .items[*]} {.status.loadBalancer.ingress[*].ip} {end}')
 #putting the external address into the goldengate deployment
-sed -i "s/ggate.141.147.33.9/ggfabric.${EXTIP// /}/g" ggfabric.yaml
+sed -i "s/ggate.xxx-xxx-xxx-xxx/ggfabric.${EXTIP// /}/g" ggfabric.yaml
 
 #sample container pull secret, only required for GG bigdata
 #  kubectl create secret docker-registry container-registry-secret -n microhacks  --docker-username=marcel.pfeifer@oracle.com --docker-password=yadayada --docker-server=container-registry.oracle.com
