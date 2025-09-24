1aef3397544e74a4e0c5f9344323159115e3da5a
ilfur <35766086+ilfur@users.noreply.github.com>
2025-09-24 10:59:58 +0200

    Update 02_oraoperator.sh

--- k8s_install/02_oraoperator.sh
+++ k8s_install/02_oraoperator.sh
@@ -1,3 +1,4 @@
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml
 kubectl apply -f https://raw.githubusercontent.com/oracle/oracle-database-operator/refs/heads/main/rbac/cluster-role-binding.yaml
+kubectl apply -f https://raw.githubusercontent.com/oracle/oracle-database-operator/refs/heads/main/rbac/node-rbac.yaml
 kubectl apply -f https://raw.githubusercontent.com/oracle/oracle-database-operator/refs/heads/main/oracle-database-operator.yaml
