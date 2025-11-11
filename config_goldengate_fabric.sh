8ef22cced6d3161e5ec954ac5800ef0e85e96d12
ilfur <35766086+ilfur@users.noreply.github.com>
2025-11-11 15:21:40 +0100

    Update config_goldengate_fabric.sh

--- config_goldengate_fabric.sh
+++ config_goldengate_fabric.sh
@@ -1,15 +1,17 @@
 #/bin/bash
 ## Variables used:
-## GG_URL     - GoldenGate (k8s internal) URL for REST API access
+## GG_URL     - GoldenGate (k8s internal) URL for REST API access, WITHOUT protocol but with port
+## GG_PROTOCOL- GoldenGate server protocol, http or https
 ## GG_URL     - GoldenGate for MS Fabric (k8s internal) URL for REST API access
 ## GG_USER    - GoldenGate REST API username, like oggadmin
 ## GG_PWD     - GoldenGate REST API password, like Welcome1234#
-
+## GG_DEPL    - GoldenGate deployment name
+export GG_PROTOCOL=http
 echo " "
 echo " "	 
 echo "Creating GG fabric credentials"
 curl -X POST \
-       $GG_URL/services/v2/credentials/Network/ggadminfabric \
+       $GG_PROTOCOL://$GG_URL/services/v2/credentials/Network/ggadminfabric \
        --user $GG_USER:$GG_PWD   \
        --insecure \
        -H 'Cache-Control: no-cache' \
@@ -20,7 +22,7 @@ curl -X POST \
 while [ $? == "7" ]; do  
 echo "RETRY - Creating GG fabric credentials"
 curl -X POST \
-       $GG_URL/services/v2/credentials/Network/ggadminfabric \
+       $GG_PROTOCOL://$GG_URL/services/v2/credentials/Network/ggadminfabric \
        --user $GG_USER:$GG_PWD   \
        --insecure \
        -H 'Cache-Control: no-cache' \
@@ -35,111 +37,120 @@ echo "creating distribution path from GGoracle to GGfabric"
 echo " "	 
 echo "creating fabric replicat"
 curl -X POST \
-       $GGFAB_URL/services/v2/replicats/RS \
+       $GG_PROTOCOL://$GG_URL/services/v2/sources/gghack2ggfabric \
        --user $GG_USER:$GG_PWD   \
        --insecure \
        -H 'Cache-Control: no-cache' \
        -d '{
-    "credentials": {
-      "alias": "srcCred",
-      "domain": "OracleGoldenGate"
-    },
-    "begin": "now",
-    "encryptionProfile": "LocalWallet",
-    "managedProcessSettings": "ogg:managedProcessSettings:Default",
-    "config": [
-      "REPLICAT RS",
-      "USERIDALIAS srcCred DOMAIN OracleGoldenGate",
-      "ALLOWNOOPUPDATES",
-	"",
-	"DBOPTIONS ENABLE_INSTANTIATION_FILTERING",
-	"",
-	"DDLERROR DEFAULT ABEND",
-	"",
-	"REPERROR (DEFAULT,RETRYOP MAXRETRIES 1)",
-	"REPERROR (26960, DISCARD)",
-	"REPERROR (PROCEDURE, DISCARD)",
-	"",
-	"DDL           INCLUDE MAPPED",
-	"DDLOPTIONS REPORT",
-	"",
-	"REPORTCOUNT EVERY 15 MINUTES, RATE",
-	"",
-	"MAPINVISIBLECOLUMNS",
-	"",
-	"MAP '$TRG_SCHEMA'.*, TARGET '$SRC_SCHEMA'.*;"
-    ],
+    "name": "gghack2ggfabric",
     "source": {
-      "name": "ET"
+      "uri": "trail://'$GG_URL'/services/'$GG_DEPL'/distsrvr/v2/sources?trail=ET",
+      "details": {
+        "encryption": {
+          "algorithm": "NONE"
+        }
+      }
     },
-    "checkpoint": {
-      "table": "GGADMIN.CHECKPOINTS"
+    "target": {
+      "uri": "ws://'$GGFAB_URL'/services/'$GG_DEPL'/recvsrvr/v2/targets?trail=ET",
+      "authenticationMethod": {
+				"domain": "Network",
+				"alias": "ggadminfabric"
+			},
+			"details": {
+				"trail": {
+					"seqLength": 9,
+					"sizeMB": 500
+				},
+				"encryption": {
+					"algorithm": "NONE"
+				},
+				"compression": {
+					"enabled": false
+				}
+			}
     },
-    "mode": {
-      "type": "integrated",
-      "parallel": false
-    }
-   }'
+    "options": {
+      "eofDelayCSecs": 10,
+      "checkpointFrequency": 10,
+      "critical": false,
+      "autoRestart": {
+        "retries": 10,
+        "delay": 2
+      },
+      "streaming": true
+    },
+    "begin": {
+      "sequence": 0,
+      "offset": 0
+    },
+    "encryptionProfile": "LocalWallet",
+    "$schema": "ogg:distPath"
+}'
    
 while [ $? == "7" ]; do  
-echo "RETRY - creating fabric replicat"
 curl -X POST \
-       $GGFAB_URL/services/v2/replicats/RS \
+       $GG_PROTOCOL://$GG_URL/services/v2/sources/gghack2ggfabric \
        --user $GG_USER:$GG_PWD   \
        --insecure \
        -H 'Cache-Control: no-cache' \
        -d '{
-    "credentials": {
-      "alias": "srcCred",
-      "domain": "OracleGoldenGate"
-    },
-    "begin": "now",
-    "encryptionProfile": "LocalWallet",
-    "managedProcessSettings": "ogg:managedProcessSettings:Default",
-    "config": [
-      "REPLICAT RS",
-      "USERIDALIAS srcCred DOMAIN OracleGoldenGate",
-      "ALLOWNOOPUPDATES",
-	"",
-	"DBOPTIONS ENABLE_INSTANTIATION_FILTERING",
-	"",
-	"DDLERROR DEFAULT ABEND",
-	"",
-	"REPERROR (DEFAULT,RETRYOP MAXRETRIES 1)",
-	"REPERROR (26960, DISCARD)",
-	"REPERROR (PROCEDURE, DISCARD)",
-	"",
-	"DDL           INCLUDE MAPPED",
-	"DDLOPTIONS REPORT",
-	"",
-	"REPORTCOUNT EVERY 15 MINUTES, RATE",
-	"",
-	"MAPINVISIBLECOLUMNS",
-	"",
-	"MAP '$TRG_SCHEMA'.*, TARGET '$SRC_SCHEMA'.*;"
-    ],
+    "name": "gghack2ggfabric",
     "source": {
-      "name": "ET"
+      "uri": "trail://'$GG_URL'/services/'$GG_DEPL'/distsrvr/v2/sources?trail=ET",
+      "details": {
+        "encryption": {
+          "algorithm": "NONE"
+        }
+      }
     },
-    "checkpoint": {
-      "table": "GGADMIN.CHECKPOINTS"
+    "target": {
+      "uri": "ws://'$GGFAB_URL'/services/'$GG_DEPL'/recvsrvr/v2/targets?trail=ET",
+      "authenticationMethod": {
+				"domain": "Network",
+				"alias": "ggadminfabric"
+			},
+			"details": {
+				"trail": {
+					"seqLength": 9,
+					"sizeMB": 500
+				},
+				"encryption": {
+					"algorithm": "NONE"
+				},
+				"compression": {
+					"enabled": false
+				}
+			}
     },
-    "mode": {
-      "type": "integrated",
-      "parallel": false
-    }
-   }'
+    "options": {
+      "eofDelayCSecs": 10,
+      "checkpointFrequency": 10,
+      "critical": false,
+      "autoRestart": {
+        "retries": 10,
+        "delay": 2
+      },
+      "streaming": true
+    },
+    "begin": {
+      "sequence": 0,
+      "offset": 0
+    },
+    "encryptionProfile": "LocalWallet",
+    "$schema": "ogg:distPath"
+}'
 done
 echo " "	 
-echo "starting fabric replicat"
-curl -X PATCH $GGFAB_URL/services/v2/replicats/RS \
+echo "starting distpath"
+curl -X PATCH $GGFAB_URL/services/v2/replicats/ET \
        --user $GG_USER:$GG_PWD   \
        --insecure \
        -H 'Cache-Control: no-cache' \
        -d '{"status": "running"}' 
        
 while [ $? == "7" ]; do  
-echo "RETRY - starting fabric replicat"
+echo "RETRY - starting distpath"
 curl -X PATCH $GGFAB_URL/services/v2/replicats/RS \
        --user $GG_USER:$GG_PWD   \
        --insecure \
