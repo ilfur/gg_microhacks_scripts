af0bbd0905a3b94af19214e62c9e5891f2fd3d41
ilfur <35766086+ilfur@users.noreply.github.com>
2025-11-12 14:27:35 +0100

    Fix URL and JSON structure in properties upload

--- config_goldengate_fabric.sh
+++ config_goldengate_fabric.sh
@@ -225,33 +225,33 @@ done
 echo " "
 echo "uploading properties file"
 curl -X POST \
-       $GG_PROTOCOL://$GGFAB_URL/services/v2//config/files/FABRIC.properties \
+       $GG_PROTOCOL://$GGFAB_URL/services/v2/config/files/FABRIC.properties \
        --user $GG_USER:$GG_PWD   \
        --insecure \
        -H 'Cache-Control: no-cache' \
-       -d '{
-	   "$schema": "ogg:config",
-       "lines": [
-			"gg.target=fabric_lakehouse",
-			"#TODO: format can be 'parquet' or 'orc' or one of the pluggable formatter types. Default is 'parquet'.",
-			"gg.format=parquet",
-			"#TODO: Edit the Fabric workspace name.",
-			"gg.eventhandler.onelake.workspace=mh_odaa",
-			"#TODO: Edit the Fabric lakehouse name.",
-			"gg.eventhandler.onelake.lakehouse=odaa_lh",
-			"#TODO: Edit the tenant ID of the application.",
-			"gg.eventhandler.onelake.tenantId=f71980b2-590a-4de9-90d5-6fbc867da951",
-			"#TODO: Edit the client ID of the application.",
-			"gg.eventhandler.onelake.clientId=ee88bf99-6dff-4025-b8c3-82f79102886a",
-			"#TODO: Edit the client secret for the authentication.",
-			"gg.eventhandler.onelake.clientSecret=2BN8Q~i5CkbyDP4QxgqIqIgw-_PkA3XKuLkEmb2E",
-			"#TODO: Edit the classpath to include Hadoop, Parquet, and Azure DataLake SDK dependencies.",
-			"gg.classpath=/mnt/deps/onelake/*:/mnt/deps/hadoop_3.4.2/*:/mnt/deps/parquet_1.16.0/*:/mnt/deps/azure-storage-blob_12.25.3/*:",
-			"#TODO: Edit the proxy configuration.",
-			"#jvm.bootoptions=-Dhttps.proxyHost=some-proxy-address.com -Dhttps.proxyPort=80 -Djava.net.useSystemProxies=true",
-			"#Mapping Parameters to create files and directories",
-			"gg.eventhandler.onelake.pathMappingTemplate=\${catalogname}.lakehouse/Files/ogg/\${groupName}/\${schemaname}.schema/\${tablename}"
-		]}'
+       -d '{"lines": [
+"gg.target=fabric_lakehouse",
+"#TODO: format can be 'parquet' or 'orc' or one of the pluggable formatter types. Default is 'parquet'.",
+"gg.format=parquet",
+"#TODO: Edit the Fabric workspace name.",
+"gg.eventhandler.onelake.workspace=mh_odaa",
+"#TODO: Edit the Fabric lakehouse name.",
+"gg.eventhandler.onelake.lakehouse=odaa_lh",
+"#TODO: Edit the tenant ID of the application.",
+"gg.eventhandler.onelake.tenantId=f71980b2-590a-4de9-90d5-6fbc867da951",
+"#TODO: Edit the client ID of the application.",
+"gg.eventhandler.onelake.clientId=ee88bf99-6dff-4025-b8c3-82f79102886a",
+"#TODO: Edit the client secret for the authentication.",
+"gg.eventhandler.onelake.clientSecret=2BN8Q~i5CkbyDP4QxgqIqIgw-_PkA3XKuLkEmb2E",
+"#TODO: Edit the classpath to include Hadoop, Parquet, and Azure DataLake SDK dependencies.",
+"gg.classpath=/mnt/deps/onelake/*:/mnt/deps/hadoop_3.4.2/*:/mnt/deps/parquet_1.16.0/*:/mnt/deps/azure-storage-blob_12.25.3/*:",
+"#TODO: Edit the proxy configuration.",
+"#jvm.bootoptions=-Dhttps.proxyHost=some-proxy-address.com -Dhttps.proxyPort=80 -Djava.net.useSystemProxies=true",
+"#Mapping Parameters to create files and directories",
+"gg.eventhandler.onelake.pathMappingTemplate=${catalogname}.lakehouse/Files/ogg/${groupName}/${schemaname}.schema/${tablename}"
+],
+"$schema": "ogg:config"
+}'
 		
 while [ $? == "7" ]; do
 echo " "
